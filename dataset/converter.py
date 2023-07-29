c18e08470f9112fd0c245baed4f5811960f03ad4
bonj4 <omerfarukkaraboga@gmail.com>
2023-07-29 15:59:05 +0300

    edited convert.py

--- dataset/converter.py
+++ dataset/converter.py
@@ -1,19 +1,31 @@
-def convert_polygon_to_yolo_bbox(polygon_vertices, class_index):
-    # Calculate bounding box coordinates
-    min_x, min_y = min(polygon_vertices, key=lambda vertex: vertex[0])[0], min(polygon_vertices, key=lambda vertex: vertex[1])[1]
-    max_x, max_y = max(polygon_vertices, key=lambda vertex: vertex[0])[0], max(polygon_vertices, key=lambda vertex: vertex[1])[1]
+def read_text_file_to_numpy(file_path):
+    data = []
 
-    # Calculate bounding box parameters
-    x_center = (min_x + max_x) / 2.0
-    y_center = (min_y + max_y) / 2.0
-    width = max_x - min_x
-    height = max_y - min_y
+    # Step 1: Read the text file line by line
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Step 2: Split each line into individual elements
+            elements = line.strip().split()
+            # Step 3: Convert the elements into numerical values (if needed)
+            numerical_elements = [float(element) for element in elements]  # Convert to float, change data type if needed
 
-    # Return the YOLO bbox annotation string
-    return f"{class_index} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}"
+            className,polygon_vertices = numerical_elements[0],numerical_elements[1:]
+            min_x, min_y = min(polygon_vertices[::2]), min(polygon_vertices[1::2])
+            max_x, max_y = max(polygon_vertices[::2]), max(polygon_vertices[1::2])
 
-# Example usage:
-class_index = 0
-polygon_vertices=[]
-yolo_bbox_annotation = convert_polygon_to_yolo_bbox(polygon_vertices, class_index)
-print(yolo_bbox_annotation)
+            # Calculate bounding box parameters
+            x_center = (min_x + max_x) / 2.0
+            y_center = (min_y + max_y) / 2.0
+            width = max_x - min_x
+            height = max_y - min_y
+
+            # Return the YOLO bbox annotation string
+            print(f"{x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}")
+            data.append([x_center,y_center,width,height,className])
+    return data
+
+
+# Usage example:
+file_path = '0.txt'  # Replace 'data.txt' with the path to your text file
+numpy_array = read_text_file_to_numpy(file_path)
+print(numpy_array)
